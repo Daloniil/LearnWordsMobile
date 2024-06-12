733bdaf9aa5d23f79aa1ea845e043207d502c85d
Daloniil <lepetinskaa@yahoo.com>
2024-06-12 23:25:15 +0300

    server

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,33 +1,82 @@
+// import FontAwesome from '@expo/vector-icons/FontAwesome';
+// import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
+// import {useFonts} from 'expo-font';
+// import * as SplashScreen from 'expo-splash-screen';
+// import {useEffect} from 'react';
+// import 'react-native-reanimated';
+// import {useColorScheme} from '@/components/useColorScheme';
+// import {createNativeStackNavigator} from "@react-navigation/native-stack";
+// import TabsLayout from "@/app/(tabs)/_layout";
+// import TabsaComponent from "@/app/(tabsa)/_layout";
+//
+// export {ErrorBoundary} from 'expo-router';
+//
+// // Создаем стек навигации
+// const Stack = createNativeStackNavigator();
+//
+// export const unstable_settings = {
+//     initialRouteName: '(tabsa)',
+// };
+//
+// SplashScreen.preventAutoHideAsync();
+//
+// export default function RootLayout() {
+//     const colorScheme = useColorScheme();
+//
+//     const [loaded, error] = useFonts({
+//         SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+//         ...FontAwesome.font,
+//     });
+//
+//     useEffect(() => {
+//         if (error) throw error;
+//     }, [error]);
+//
+//     useEffect(() => {
+//         if (loaded) {
+//             SplashScreen.hideAsync();
+//         }
+//     }, [loaded]);
+//
+//     if (!loaded) {
+//         return null;
+//     }
+//
+//     return (
+//         <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+//             <Stack.Navigator initialRouteName='(tabsa)' screenOptions={{headerShown: true}}>
+//                 <Stack.Screen name="(tabsa)" component={TabsaComponent} options={{headerShown: false}}/>
+//                 <Stack.Screen name="(tabs)" component={TabsLayout} options={{headerShown: false}}/>
+//             </Stack.Navigator>
+//         </ThemeProvider>
+//     );
+// }
+
+
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
 import {useFonts} from 'expo-font';
+import {Stack} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import {useEffect} from 'react';
 import 'react-native-reanimated';
+
 import {useColorScheme} from '@/components/useColorScheme';
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import TabsLayout from "@/app/(tabs)/_layout";
-import TabsaComponent from "@/app/(tabsa)/_layout";
 
 export {ErrorBoundary} from 'expo-router';
 
-// Создаем стек навигации
-const Stack = createNativeStackNavigator();
+export const unstable_settings = {initialRouteName: '(tabsa)/index'};
 
-export const unstable_settings = {
-    initialRouteName: '(tabsa)',
-};
-
+// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-    const colorScheme = useColorScheme();
-
     const [loaded, error] = useFonts({
         SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
         ...FontAwesome.font,
     });
 
+    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
     useEffect(() => {
         if (error) throw error;
     }, [error]);
@@ -42,12 +91,19 @@ export default function RootLayout() {
         return null;
     }
 
+    return <RootLayoutNav/>;
+}
+
+function RootLayoutNav() {
+    const colorScheme = useColorScheme();
+
     return (
         <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-            <Stack.Navigator initialRouteName='(tabsa)' screenOptions={{headerShown: true}}>
-                <Stack.Screen name="(tabsa)" component={TabsaComponent} options={{headerShown: false}}/>
-                <Stack.Screen name="(tabs)" component={TabsLayout} options={{headerShown: false}}/>
-            </Stack.Navigator>
+            <Stack>
+                <Stack.Screen name="(tabsa)" options={{headerShown: false}}/>
+                <Stack.Screen name="modal" options={{presentation: 'modal'}}/>
+                <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+            </Stack>
         </ThemeProvider>
     );
 }
